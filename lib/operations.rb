45df6c6c21574bcbe0d7a65396f7e790b5d8e34b
Andre Pierre <andre.laurence94@gmail.com>
2021-05-31 23:46:31 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -5,8 +5,20 @@ end
 
 
 def not_safe?(speed)
-  
+  speed >= 40 && speed <= 60 ? true : false
 end
 
 
-
+=begin
+if speed >= 40 && speed <= 60
+    return false
+    
+    elsif speed > 60 
+    return true
+    
+  else
+    speed < 40 
+    return true
+    
+  end 
+=end 
